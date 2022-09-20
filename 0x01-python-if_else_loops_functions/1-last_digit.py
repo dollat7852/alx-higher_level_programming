e298af50ff7728506a40a2b2876d8aeda3b88180
Dollat <dollat4real@gmail.com>
2022-09-20 12:25:44 -0700

    fixed formatting error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ ld = abs(number) % 10
 if (number < 0):
     ld *= -1
 if (ld > 5):
-    print(f"Last digit of {number:d} is {ld:d} and is greater than  5")
+    print(f"Last digit of {number:d} is {ld:d} and is greater than 5")
 elif (ld == 0):
     print(f"Last digit of {number:d} is {ld:d} and is 0")
 else:
