ec224325b79e0e51b32806a6eec8c11f6a4e9a3f
Dollat <dollat4real@gmail.com>
2022-09-20 05:29:56 -0700

    added some more tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if 2 consecutive elements of a list points to each other
+ * @list: list to be checked
+ * Return: 0 if no repetition, 1 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+	/* declare placeholders */
+	listint_t *current = list;
+	listint_t *tmp;
+
+	if (current == NULL) /* an empty list cannot be cycle*/
+		return (0);
+	tmp = current->next;
+
+	while (tmp != NULL && tmp->next != NULL)
+	{
+		if (current == tmp)
+			return (1);
+		current = tmp;
+		tmp = tmp->next;
+	}
+	return (0);
+}
