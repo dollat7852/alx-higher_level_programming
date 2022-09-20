42e93ce0b218d78ed97ebda33420849ca6675339
Dollat <dollat4real@gmail.com>
2022-09-20 12:23:49 -0700

    fixing pycode style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 ld = abs(number) % 10
 if (number < 0):
-	ld *= -1
+    ld *= -1
 if (ld > 5):
-	print(f"Last digit of {number:d} is {ld:d} and is greater than  5")
+    print(f"Last digit of {number:d} is {ld:d} and is greater than  5")
 elif (ld == 0):
-	print(f"Last digit of {number:d} is {ld:d} and is 0")
+    print(f"Last digit of {number:d} is {ld:d} and is 0")
 else:
-	print(f"Last digit of {number:d} is {ld:d} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {ld:d} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(97, 97 + 26):
-        print("{ch:c}".format(ch=i), end='')
+    print("{ch:c}".format(ch=i), end='')
