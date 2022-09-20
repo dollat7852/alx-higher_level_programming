2438e3695a0284aa0a259fbd80664ba6d6f13371
Dollat <dollat4real@gmail.com>
2022-09-20 11:55:38 -0700

    initial commit

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,2 @@
+ in range(97, 97 + 26):
+    print(f"{i:c}", end='')
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for i in range(97, 97 + 26):
+    if f"{i:c}" in (e,q):
+        continue
+    print("{ch:c}".format(ch=i), end='')
