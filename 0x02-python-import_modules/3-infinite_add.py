3168ee1ff78596672fa590f84cd50fc96df8e720
Dollat <dollat4real@gmail.com>
2022-09-22 11:36:31 -0700

    some fixes on task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,7 @@ import sys
 if __name__ == '__main__':
     args = sys.argv
     args.pop(0)
+    total = 0
 for arg in args:
-    sum += int(arg)
-print("{:d}".format(sum))
+    total += int(arg)
+print("{:d}".format(total))
