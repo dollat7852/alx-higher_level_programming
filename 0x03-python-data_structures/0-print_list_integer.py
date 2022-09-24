bc21cbce162b09f206c904a919dcb74d07888a2b
Dollat <dollat4real@gmail.com>
2022-09-24 14:47:20 -0700

    debug

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if __name__ == '__main__':
-        for x in range(len(my_list)-1, -1, -1):
-            print("{:d}".format(my_list[x]))
+    for x in range(len(my_list)-1, -1, -1):
+        print("{:d}".format(my_list[x]))
