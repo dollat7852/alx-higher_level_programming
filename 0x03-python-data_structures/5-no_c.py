478e345ff0db2615c9756c1e7f106d151089abe1
Dollat <dollat4real@gmail.com>
2022-09-25 04:48:47 -0700

    trying typecasting with list comprehension

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     filtered = ''
-    for x in my_string:
-        if x not in 'cC':
-            filtered += x
-    return (filtered)
+    [filtered + x for x in my_string if x not in 'cC']
+    return (str(filtered))
