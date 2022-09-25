83a5684b1bc358fbc1923f43e6d819a5fc2a6efb
Dollat <dollat4real@gmail.com>
2022-09-25 04:33:47 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     filtered = ''
-    [filtered + x for x in my_string if x not in 'Cc']
+    [(filtered + x) for x in my_string if x not in 'Cc']
     return (filtered)
