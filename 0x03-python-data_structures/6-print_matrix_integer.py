ebcd5f1c75c4ec7d4afcd960f9f6738f5bce4402
Dollat <dollat4real@gmail.com>
2022-09-25 04:59:31 -0700

    t6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for row in matrix:
         for i in row:
-            print("{:d} ".format(i))
+            print("{:d}".format(i))
         print("\n")
