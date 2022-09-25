50f54b69beddaec3e3bd854dd89df930dd3a2f03
Dollat <dollat4real@gmail.com>
2022-09-25 05:24:03 -0700

    task  6 requires .format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return None
     for row in matrix:
-        fmt =" ".join([i for i in row])
+        fmt =" ".join(["{:d}".format(i) for i in row])
         print(fmt.format(*row))
