2f04f6d5f52d90cc47f9125bbcdacf13602be764
Dollat <dollat4real@gmail.com>
2022-09-25 05:38:09 -0700

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    while len(a) < 2:
+        a = (*a, 0)
+    while len(b) < 2:
+        b = (*b, 0)
+    return (a[0] + b[0], a[1] + b[1])
