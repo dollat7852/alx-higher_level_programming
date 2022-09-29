d11ac09b4c71ce224826d6931a51582a08e510a3
Dollat <dollat4real@gmail.com>
2022-09-29 02:25:25 -0700

    T102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     if a_dictionary is None:
         return None
-    for key in a_dictionary:
+    for key in a_dictionary.keys():
         if a_dictionary[key] == value:
             del(a_dictionary[key])
     return a_dictionary
